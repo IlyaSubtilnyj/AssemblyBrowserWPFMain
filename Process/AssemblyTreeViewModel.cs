57d4d247dd26baabb8d2adf7503499e9e990d83e
IlyaGonzarevich <i0treret@gmail.com>
2024-05-12 12:44:08 +0300

    Добавьте файлы проекта.

---
+++ Process/AssemblyTreeViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace AssemblyBrowserLab
+{
+    internal class AssemblyTreeViewModel
+    {
+        public AssemblyTreeViewModel() { }
+
+        private string? _assemblyName;
+        public string? SourceName
+        {
+            get { return _assemblyName; }
+            set
+            {
+                if (value == null)
+                    return;
+
+                _assemblyName = value;
+                MessageBox.Show(_assemblyName);             
+            }
+        }
+    }
+}
---
+++ Process/OpenDialogCommand.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AssemblyBrowserLab
+{
+    internal class OpenDialogCommand : ICommand
+    {
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            if (parameter is AssemblyTreeViewModel dataContext)
+            {
+                var openFileDialog = new Microsoft.Win32.OpenFileDialog();
+                openFileDialog.Filter = "Assembly Files (*.dll;*.exe)|*.dll;*.exe";
+                if (openFileDialog.ShowDialog() == true)
+                {
+                    var selectedFilePath = openFileDialog.FileName;
+                    dataContext.SourceName = selectedFilePath;
+                }
+            }
+        }
+    }
+}
